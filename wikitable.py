e619b9f397d866f814d86e86fa26aeccf2c7997d
Junwoo Seo <junwoo93s@gmail.com>
2018-01-30 12:52:17 -0500

    Update wikitable.py

--- wikitable.py
+++ wikitable.py
@@ -1,28 +1,27 @@
-# for 100
-# wiki data
-# https://en.wikipedia.org/wiki/List_of_United_States_counties_and_county_equivalents
-# https://en.wikipedia.org/wiki/List_of_counties_by_U.S._state
-# https://www.census.gov/geo/reference/geoguide.html
-# https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt
+#Author : Junwoo Seo
 
 import csv
-#import urllib.request
+import urllib.request
 import urllib2
-#from urllib2 import urlopen
+from urllib2 import urlopen
 from bs4 import BeautifulSoup
 
-#f = open('Alabama.csv', 'w', newline='')
-#writer = csv.writer(f)
+f = open('FILENAME.csv', 'w', newline='') #this is where you open the excel file that you want to transferred into. 
+writer = csv.writer(f)
 
-with open("Alabama.csv", "wb") as f:
-    w = csv.DictWriter(f, fieldnames=["Difference", "NumNegativePorter", "NumPositivePorter", "dsk", "pubdate", "pubmon", "statementNumber"])
+with open("FILENAME.csv", "wb") as f: #FILENAME is where your file name goes in. 
+    w = csv.DictWriter(f, fieldnames=["COULMNS NAME IN YOUR EXCEL"]) #you can list the colunms that you want to be saved 
     w.writeheader()
 
 
-soup = BeautifulSoup(urllib2.urlopen("https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_Alabama").read(), 'lxml')
-tbody = soup('table', {"class": "wikitable sortable jquery-tablesorter"})[0].find_all('tr')
-for row in tbody:
-    cols = row.findChildren(recursive=False)
-    cols = [ele.text.strip() for ele in cols]
-    writer.writerow(cols)
-    print(cols)
+scrap = BeautifulSoup(urllib2.urlopen("https://en.wikipedia.org/wiki/List_of_cities_and_towns_in_Alabama").read(), 'lxml')
+table_body = scrap('table', {"class": "wikitable sortable jquery-tablesorter"})[0].find_all('tr')
+for row in table_body:
+    colums = row.findChildren(recursive=False)
+    colums = [ele.text.strip() for ele in colums]
+    writer.writerow(colums)
+    print(colums)
+
+    
+    
+    
